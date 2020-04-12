b54aae36022abb6766ce927a032b08d501e46d22
Iván <ivan@iMAC27.home>
2020-04-12 21:06:42 +0200

    Sección 12 terminada - backend terminado

--- app.js
+++ app.js
@@ -22,13 +22,15 @@ var uploadRoutes = require('./routes/upload');
 var imagenesRoutes = require('./routes/imagenes');
 
 // Conexión a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res ) => {
+// mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res ) => {
 
-    if ( err ) throw err;
+//     if ( err ) throw err;
 
-    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+//     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
-});
+// });
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb://localhost:27017/hospitalDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Server index config
 // var serveIndex = require('serve-index');
--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+
+// Google
+module.exports.CLIENT_ID = '154102253880-l31tau3jq5qfvpc9c7gl4t3mbi978n2d.apps.googleusercontent.com';
